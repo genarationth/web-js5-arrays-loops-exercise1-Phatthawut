c4e39787e3d56abbcc9fce11941defa35b5c1722
Phatthawut <42006909+Phatthawut@users.noreply.github.com>
2024-05-15 13:37:14 +0700

    Create script for array-loop exercise 1

---
+++ script.js
@@ -0,0 +1,15 @@
+let myAlphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G'];
+
+myAlphabet.forEach(alp => {
+    console.log(alp);
+    })
+    console.log(myAlphabet.length);;
+    
+
+let solarPlanet = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"];
+console.log(solarPlanet);
+
+let myArr = [ 1, 2, 'One', true];
+    myArr.forEach(element => {
+        console.log(element);
+    });
